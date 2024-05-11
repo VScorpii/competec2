7a3fa9713c109baad4bb8b7c959b63754eb7d3c5
Victor Scorpii <166924880+VScorpii@users.noreply.github.com>
2024-05-11 14:35:07 +0000

    aula3 + demais atividades

--- src/main/java/competec/desafio_switch_case/desafio4.java
+++ src/main/java/competec/desafio_switch_case/desafio4.java
@@ -4,22 +4,22 @@ public class desafio4 {
     public static void main(String[] args) {
         Scanner read = new Scanner(System.in);
         int codigo = 0;
-        float precoAbacaxi = 8.5;
-        float precoGoiaba = 2.0;
-        float precoLaranja = 1.3;
+        float precoAbacaxi = 8.5f;
+        float precoGoiaba = 2.0f;
+        float precoLaranja = 1.3f;
 
         System.out.print("Digite o código para solicitar o preço do produto: ");
-        codigo = read.nextInt()
+        codigo = read.nextInt();
         
         switch (codigo) {
             case 1:
-                System.out.println("Código " + codigo + " Abacaxi - " + preco);
+                System.out.println("Código " + codigo + " Abacaxi - " + precoAbacaxi);
                 break;
             case 2:
-                System.out.println("Código " + codigo + " Goiaba - " + preco);
+                System.out.println("Código " + codigo + " Goiaba - " + precoGoiaba);
                 break;
             case 3:
-                System.out.println("Código " + codigo + " Laranja - " + preco);
+                System.out.println("Código " + codigo + " Laranja - " + precoLaranja);
                 break;
             default:
                 System.out.println("Código de produto inexistente!");
